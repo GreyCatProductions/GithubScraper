95cc090b60a7c184d0ddf42566186fbd25288d9d
GreyCatProductions <GreyCatProductionsOriginal@gmail.com>
2025-06-24 09:05:54 +0200

    refactoring

--- GitHubTokenReader.py
+++ GitHubTokenReader.py
@@ -3,8 +3,10 @@ from Logger import log
 def get_tokens():
     tokens = []
     try:
-        with open('GitHubTokens', 'r', encoding='utf-8') as file:
+        with open('./keys/GitHubTokens', 'r', encoding='utf-8') as file:
             for line in file:
+                if "#" in line:
+                    continue
                 tokens.append(line.strip())
     except Exception as e:
         log(-1, "ERROR", f"Failed to read tokens {e}")
